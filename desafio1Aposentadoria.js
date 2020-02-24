aab44daf613cd56e51bbe8107d027c29ee49de6d
DanOliveira <61166364+danoliveira86@users.noreply.github.com>
2020-02-24 17:11:30 -0300

    Add files via upload

---
+++ desafio1Aposentadoria.js
@@ -0,0 +1,17 @@
+const nome = "Joaribia";
+const sexo = "F";
+const idade = 56;
+const contribuicao = 35;
+
+
+const calculoContribuicao = idade + contribuicao
+
+// essas variáveis irão retornar true ou false
+const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+if ( homemPodeAposentar || mulherPodeAposentar) {
+    console.log(`${nome}, você pode se aposentar!`)
+} else {
+    console.log(`${nome}, você não pode se aposentar!`)
+}
